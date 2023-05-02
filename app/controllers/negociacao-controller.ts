49468bb187f7dcbc52f933926c1b01382baffccc
endreo123 <endreo.leoni@gmail.com>
2023-05-01 23:12:52 -0300

    curso 2 pt 3

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,26 +1,44 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes;
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView')
+    private SABADO = 6;
+    private DOMINGO = 0;
 
     constructor(){
         this.inputData = document.querySelector("#data");
         this.inputQuantidade = document.querySelector("#quantidade");
         this.inputValor = document.querySelector("#valor");
+        this.negociacoesView.update(this.negociacoes);
     }
 
-    adiciona(): void{
+    public adiciona(): void{
         const negociacao = this.criaNegociacao();
+        const diaSemana = negociacao.data.getDay()
+
+        if(!this.ehDiaUtil(negociacao.data)){
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas')
+            return;
+        }
+
         this.negociacoes.adiciona(negociacao);
-        console.table(this.negociacoes.lista())
         this.limparFormulario();
+        this.atualizaView();
     }
 
-    criaNegociacao(): Negociacao{
+    private ehDiaUtil(data: Date){
+        return data.getDay() > this.DOMINGO && data.getDay() < this.SABADO
+    }
+
+    private criaNegociacao(): Negociacao{
         const exp = /-/g
         const data = new Date(this.inputData.value.replace(exp, ","));
         const quantidade = parseInt(this.inputQuantidade.value);
@@ -28,10 +46,15 @@ export class NegociacaoController {
         return new Negociacao(data, quantidade, value)
     }
 
-    limparFormulario(): void {
+    private limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
+    }
+
+    private atualizaView(): void {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociacao adicionada com sucesso');
     }
 }
